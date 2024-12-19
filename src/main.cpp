ca2a75359fc0067f7a77b02cd9af6ec9e27e6c32
Lilian Peuron <lilianpeuron@gmail.com>
2024-12-19 21:16:30 +0100

    Add terminal_ctrl library and enhance game logic with player move validation and winning checks

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,8 @@
 
 struct Player
 {
-    std::string name {};
-    char symbol {"X" || "O"};
+    std::string name{};
+    char symbol{"X" || "O"};
 
     void create_player();
 };
@@ -21,14 +21,13 @@ void Player::create_player()
 
     std::cout << "Nom du joueur : " << this->name << std::endl;
     std::cout << "Symbole choisi : " << this->symbol << std::endl;
-
 };
 
-void draw_game_board(std::array<char, 9>& game_board)
+void draw_game_board(std::array<char, 9> &game_board)
 {
     for (int i = 0; i < game_board.size(); i++)
-    {   
-        std::cout << "| " << game_board[i] << " " ;
+    {
+        std::cout << "| " << game_board[i] << " ";
 
         if (i == 2 || i == 5 || i == 8)
         {
@@ -36,18 +35,34 @@ void draw_game_board(std::array<char, 9>& game_board)
         }
     }
 
-    std::cout << std::endl ;
-
+    std::cout << std::endl;
 }
 
-void player_move(struct Player& player , std::array<char, 9>& game_board)
+void show_ref_board(std::array<char, 9> &game_board)
 {
-    int number {};
+    std::cout << "Aide :" << std::endl;
+
+    for (int i = 0; i < game_board.size(); i++)
+    {
+        std::cout << "| " << i + 1 << " ";
+
+        if (i == 2 || i == 5 || i == 8)
+        {
+            std::cout << "|" << std::endl;
+        }
+    }
+
+    std::cout << std::endl;
+}
+
+void player_move(struct Player &player, std::array<char, 9> &game_board)
+{
+    int number{};
     while (!number > 0 && !number < 8)
     {
         std::cout << "Entrez un nombre entre 1 et 9" << std::endl;
         std::cin >> number;
-        number --;
+        number--;
         if (game_board[number] == ' ')
         {
             game_board[number] = player.symbol;
@@ -58,13 +73,12 @@ void player_move(struct Player& player , std::array<char, 9>& game_board)
             std::cout << "DEJA PRIS" << std::endl;
         }
     }
-    
 }
 
-void AI_move_easy(std::array<char, 9>& game_board)
+void AI_move_easy(std::array<char, 9> &game_board)
 {
-    int random_number {};
-    int occupied_case {true};
+    int random_number{};
+    int occupied_case{true};
     while (occupied_case == true)
     {
         int random_number = rand() % 9;
@@ -74,42 +88,68 @@ void AI_move_easy(std::array<char, 9>& game_board)
             game_board[random_number] = 'O';
         }
     }
-
-    
 }
 
-bool is_player_winning(struct Player& player , std::array<char, 9>& game_board)
+bool is_player_winning(struct Player &player, std::array<char, 9> &game_board)
 {
-    for (int i{0}; i<=2; i++)
+    if (game_board[0] == player.symbol && game_board[1] == player.symbol && game_board[2] == player.symbol)
     {
-        if(game_board[0] == game_board[1] && game_board[1]==game_board[2] && game_board[0] == player.symbol)
-        {
-            std::cout << "Victoire de " << player.name << std::endl;
-            return true;
-        }
-        else 
-        {
-            return false;
-        }
+        std::cout << "Victoire de " << player.name << std::endl;
+        return true;
+    }
+    else if (game_board[3] == player.symbol && game_board[4] == player.symbol && game_board[5] == player.symbol)
+    {
+        std::cout << "Victoire de " << player.name << std::endl;
+        return true;
+    }
+    else if (game_board[6] == player.symbol && game_board[7] == player.symbol && game_board[8] == player.symbol)
+    {
+        std::cout << "Victoire de " << player.name << std::endl;
+        return true;
+    }
+    else if (game_board[0] == player.symbol && game_board[3] == player.symbol && game_board[6] == player.symbol)
+    {
+        std::cout << "Victoire de " << player.name << std::endl;
+        return true;
+    }
+    else if (game_board[1] == player.symbol && game_board[4] == player.symbol && game_board[7] == player.symbol)
+    {
+        std::cout << "Victoire de " << player.name << std::endl;
+        return true;
+    }
+    else if (game_board[2] == player.symbol && game_board[5] == player.symbol && game_board[8] == player.symbol)
+    {
+        std::cout << "Victoire de " << player.name << std::endl;
+        return true;
+    }
+    else if (game_board[0] == player.symbol && game_board[4] == player.symbol && game_board[8] == player.symbol)
+    {
+        std::cout << "Victoire de " << player.name << std::endl;
+        return true;
+    }
+    else if (game_board[2] == player.symbol && game_board[4] == player.symbol && game_board[6] == player.symbol)
+    {
+        std::cout << "Victoire de " << player.name << std::endl;
+        return true;
+    }
+    else
+    {
+        return false;
     }
 }
 
 int main()
-{   
-    srand (time(NULL));
+{
+    srand(time(NULL));
 
-    std::system("CLS");
-
-    int game_mode {};
+    int game_mode{};
     std::array<char, 9> game_board{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
 
     std::cout << "Bienvenue dans le jeu du TicTacToe" << std::endl;
-    std::cout << "Veuillez choisir un mode de jeu :" << std::endl ;
-    std::cout << "1. Joueur contre Joueur" << std::endl ;
-    std::cout << "2. Joueur contre IA" << std::endl ;
-    std::cin >> game_mode ;
-
-    std::system("CLS");
+    std::cout << "Veuillez choisir un mode de jeu :" << std::endl;
+    std::cout << "1. Joueur contre Joueur" << std::endl;
+    std::cout << "2. Joueur contre IA" << std::endl;
+    std::cin >> game_mode;
 
     if (game_mode == 1)
     {
@@ -119,22 +159,36 @@ int main()
         Player player2;
         player2.create_player();
 
+        draw_game_board(game_board);
+
+        while (is_player_winning(player, game_board) == false && is_player_winning(player2, game_board) == false)
+        {
+            show_ref_board(game_board);
+            player_move(player, game_board);
+            draw_game_board(game_board);
+            player_move(player2, game_board);
+            draw_game_board(game_board);
+        }
     }
     else if (game_mode == 2)
     {
         Player player;
         player.create_player();
 
+        Player AI;
+        AI.name = "IA";
+        AI.symbol = 'O';
+
         draw_game_board(game_board);
 
-        while (is_player_winning(player, game_board) == false)
+        while (is_player_winning(player, game_board) == false && is_player_winning(AI, game_board) == false)
         {
+            show_ref_board(game_board);
             player_move(player, game_board);
             AI_move_easy(game_board);
             draw_game_board(game_board);
         }
-        
     }
-    
+
     return 0;
 }
